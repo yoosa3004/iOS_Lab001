b777ae3b1712379739aebe6117a009f71623b5f1
Hyunndy <yoosa3004@deali.net>
2021-10-11 16:35:13 +0900

    Initial Commit

---
+++ iOS_Lab001/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iOS_Lab001
+//
+//  Created by hyunndy on 2021/10/11.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
